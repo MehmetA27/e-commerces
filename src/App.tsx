e92fe92225c7e0d584195f8a6bab23b0296a1a05
MAslan <m.aslan027@hotmail.com>
2024-12-11 00:53:52 +0100

    first step

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    
+      <div>
+        E Ticaret Sitesi
+      </div>
+      
+  )
+}
+
+export default App
