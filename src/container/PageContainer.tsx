dac9a631cf09aaa78866a467d96b5ff6802acce7
MAslan <m.aslan027@hotmail.com>
2025-01-08 10:31:56 +0100

    new future

--- src/container/PageContainer.tsx
+++ src/container/PageContainer.tsx
@@ -2,11 +2,7 @@ import React from "react";
 import Container from "@mui/material/Container";
 
 function PageContainer({ children }) {
-  return (
-    <div>
-      <Container maxWidth="lg">{children}</Container>
-    </div>
-  );
+  return <Container maxWidth="lg">{children}</Container>;
 }
 
 export default PageContainer;
